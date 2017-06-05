e1f60d30ad59344ff775db20828c208846517316
vpalex999 <vpalex@mail.ru>
2017-06-05 09:33:02 +0500

    фикстура для адреса

--- application.py
+++ application.py
@@ -17,6 +17,10 @@ class Application(object):
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
+    def return_home_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
+
     def create_group(self, group):
         # init group creation
         wd = self.wd
@@ -53,6 +57,94 @@ class Application(object):
         # open homepage
         wd = self.wd
         wd.get("http://localhost/addressbook/")
+
+    def open_new_address_page(self):
+        # open new address page
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
+
+    def add_new_address(self, addr):
+        # create new address
+        wd = self.wd
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(addr.name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(addr.mname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(addr.lname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(addr.nickname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(addr.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(addr.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(addr.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(addr.phone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(addr.mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(addr.workphone)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(addr.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(addr.email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(addr.email2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(addr.email3)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(addr.homepage)
+        # Birthday
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[16]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[16]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys("1991")
+        # Anniversary
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[6]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[6]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[3]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[3]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys("2017")
+        # select Group
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[5]//option[1]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[5]//option[1]").click()
+        # Secondary
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(addr.address2)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(addr.phone2)
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(addr.notes)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+
 
     def destroy(self):
         self.wd.quit()
