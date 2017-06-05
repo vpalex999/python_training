11a490fc46f7a10bcb6285ab3b3185b81bfc8bad
vpalex999 <vpalex@mail.ru>
2017-06-05 15:06:02 +0500

    Выделен класс для работы с адресами книги

--- test/test_add_address.py
+++ test/test_add_address.py
@@ -2,8 +2,8 @@
 
 import pytest
 
-from address import Address
 from fixture.application import Application
+from model.address import Address
 
 
 @pytest.fixture()
@@ -17,9 +17,9 @@ def test_add_address(app):
 
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.open_new_address_page()
-    app.add_new_address(Address("Иван", "Иванович", "Иванов", "ivan", "soft", "software", "г. Екатеринбург",
+    app.address.new_address_page()
+    app.address.create(Address("Иван", "Иванович", "Иванов", "ivan", "soft", "software", "г. Екатеринбург",
                          "8(343)2102101", "8(922)1234567", "8(343)7654321", "8(343)6543721", "ivanov@mail.ru",
                          "ivanov@ya.ru", "ivanov@rambler.ru", "ivanov.com", "г. Москва", "8(495)7654673", "йцукен"))
-    app.return_home_page()
+    app.address.return_home_page()
     app.session.logout()
