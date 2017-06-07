5e6f82a5bf47f1e2a08de0f06d43669bdeaea358
vpalex999 <vpalex@mail.ru>
2017-06-07 09:37:39 +0500

    Тест для редактирования первой выбранной группы

--- fixture/group.py
+++ fixture/group.py
@@ -15,6 +15,28 @@ class GroupHelper(object):
         wd = self.app.wd
         wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
         # fill group
+        self.input_fields(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+
+    def delete_first_group(self):
+        # select first group
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        # submit deletions
+        wd.find_element_by_name("delete").click()
+
+    def edit_first_group(self, group):
+        # edit first group
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        # edit fields
+        self.input_fields(group)
+        wd.find_element_by_name("update").click()
+
+    def input_fields(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -24,15 +46,10 @@ class GroupHelper(object):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
+        # submit group edit
 
-    def delete_first_group(self):
-        wd = self.app.wd
-        # select first group
-        wd.find_element_by_name("selected[]").click()
-        # submit deletions
-        wd.find_element_by_name("delete").click()
+
+
 
     def return_to_groups_page(self):
         # return to groups page
