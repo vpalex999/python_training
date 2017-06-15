f8534461551be670dbe07660795a9f6beaf8dd62
vpalex999 <vpalex@mail.ru>
2017-06-15 09:42:59 +0500

    Проверка наличия групп перед удаление
    + Если проверка количества групп возвратила 0, то сначала создаем группу.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from model.group import Group
 
 def test_delete_first_group(app):
 
     app.group.open_groups_page()
+    if app.group.count() == 0:
+        app.group.create(Group(name="Create group"))
+        app.group.return_to_groups_page()
     app.group.delete_first_group()
-    app.group.return_to_groups_page()
     app.group.return_home_page()
 
