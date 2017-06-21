4f3304ec6991c3cca9b8cb394bf7e4fa4132445e
oleg <bubenshchikov@iskrauraltel.ru>
2017-06-21 10:20:00 +0500

    Кеширование и хеширование списков групп

--- fixture/group.py
+++ fixture/group.py
@@ -35,6 +35,7 @@ class GroupHelper(object):
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_home_page()
+        self.group_chace = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -52,6 +53,7 @@ class GroupHelper(object):
         # submit deletions
         wd.find_element_by_name("delete").click()
         self.return_home_page()
+        self.group_chace = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -70,6 +72,7 @@ class GroupHelper(object):
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
         self.return_home_page()
+        self.group_chace = None
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -83,12 +86,15 @@ class GroupHelper(object):
         wd = self.app.wd
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_chace = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_chace is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_chace = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_chace.append(Group(name=text, id=id))
+        return list(self.group_chace)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,7 +8,7 @@ def test_add_group(app):
     group = Group(name="test group", header="new group", footer="footer")
     app.group.create(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups)+1 == len(new_groups)
+    assert len(old_groups)+1 == app.group.count()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
@@ -18,7 +18,7 @@ def test_add_empty_group(app):
     group = Group(name="", header="", footer="")
     app.group.create(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+    assert len(old_groups) + 1 == app.group.count()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
