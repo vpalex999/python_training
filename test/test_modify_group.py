8c8e820496fbe216279faef4129dfd07f20d75a6
oleg <bubenshchikov@iskrauraltel.ru>
2017-06-28 11:37:30 +0500

    убрал ненужный тест для групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -38,21 +38,6 @@ def test_modify_group_name(app, group):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-# def test_modify_group_header(app):
-#     if not app.group.count():
-#         app.group.create(Group(name="Test test "))
-#     old_groups = app.group.get_group_list()
-#     index = randrange(len(old_groups))
-#     group = Group(name="Test test ", header="New header")
-#     app.group.modify_group_by_index(group, index)
-#     group.id = old_groups[index].id
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) == app.group.count()
-#     group.name = del_space_for_end_string(group.name)
-#     old_groups[index] = group
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
 def clean_other_char(s):
     return re.sub("[']", "", s)
 
