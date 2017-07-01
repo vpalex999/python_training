4736a04bfc0899a47fa16b814ab976bd9317d753
oleg <bubenshchikov@iskrauraltel.ru>
2017-07-01 21:48:22 +0500

    тест home page

--- test/test_home_page.py
+++ test/test_home_page.py
@@ -2,50 +2,66 @@
 
 import re
 from random import randrange
+import random
+import string
+import pytest
 from model.address import Address
 
 
-def test_view_contact_from_home_page(app):
-    if not app.address.count():
-        app.address.app.address.new_address_page()
-        app.address.create(Address("Иван", "Иванович", "Иванов", "ivan", "soft", "software", "г. Екатеринбург",
-                                   "8(343)2102101", "8(922)1234567", "8(343)7654321", "8(343)6543721", "ivanov@mail.ru",
-                                   "ivanov@ya.ru", "ivanov@rambler.ru", "ivanov.com", "г. Москва", "8(495)7654673",
-                                   "йцукен"))
-        app.address.return_home_page()
-    address_from_home_page = app.address.get_addresses_list()[0]
-    address_from_edit_page = app.address.get_contact_info_from_edit_page(0)
-    assert address_from_home_page.lname == address_from_edit_page.lname
-    assert address_from_home_page.name == address_from_edit_page.name
-    assert address_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(address_from_edit_page)
-    assert address_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(address_from_edit_page)
+def random_string(prefix, maxlen):
+    symbols = f"{string.ascii_letters}{string.digits}"
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-def test_view_contact_from_home_page_by_index(app):
+testdata = [Address(name="", mname="", lname="", nickname=random_string("nickname", 10))] + [
+    Address(name=random_string("name", 10), mname=random_string("mname", 10), lname=random_string("lname", 10),
+            nickname=random_string("nickname", 10),
+            title=random_string("title", 10), company=random_string("company", 10),
+            address=random_string("address", 10), phone=random_string("phone", 10),
+            mobile=random_string("mobile", 10), workphone=random_string("workphone", 10),
+            fax=random_string("fax", 10), email=random_string("email", 10), email2=random_string("email2", 10),
+            email3=random_string("email3", 10), homepage=random_string("homepage", 10),
+            address2=random_string("address2", 10), phone2=random_string("phone2", 10),
+            notes=random_string("notes", 10))
+        for i in range(5)
+    ]
+
+
+@pytest.mark.parametrize("address", testdata, ids=[repr(x) for x in testdata])
+def test_view_contact_from_home_page_by_index(app, address):
     if not app.address.count():
-        app.address.app.address.new_address_page()
-        app.address.create(Address("Иван", "Иванович", "Иванов", "ivan", "soft", "software", "г. Екатеринбург",
-                                   "8(343)2102101", "8(922)1234567", "8(343)7654321", "8(343)6543721", "ivanov@mail.ru",
-                                   "ivanov@ya.ru", "ivanov@rambler.ru", "ivanov.com", "г. Москва", "8(495)7654673",
-                                   "йцукен"))
-        app.address.return_home_page()
+        app.address.create(address)
     old_addresses = app.address.get_addresses_list()
     index = randrange(len(old_addresses))
     address_from_home_page = old_addresses[index]
     address_from_edit_page = app.address.get_contact_info_from_edit_page(index)
-    assert address_from_home_page.lname == address_from_edit_page.lname
+    if address_from_home_page.lname is None:
+        address_from_home_page.lname = ""
+    assert address_from_home_page.lname == clear(address_from_edit_page.lname)
+    if address_from_home_page.name is None:
+        address_from_home_page.name = ""
     assert address_from_home_page.name == address_from_edit_page.name
-    assert address_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(address_from_edit_page)
-    assert address_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(address_from_edit_page)
+    if address_from_home_page.all_email_from_home_page is None:
+        address_from_home_page.all_email_from_home_page = ""
+    assert clear(address_from_home_page.all_email_from_home_page) == merge_email_like_on_home_page(
+        address_from_edit_page)
+    if address_from_home_page.all_phones_from_home_page is None:
+        address_from_home_page.all_phones_from_home_page = ""
+    assert clear(address_from_home_page.all_phones_from_home_page) == merge_phones_like_on_home_page(
+        address_from_edit_page)
 
 
 def clear_phone(s):
     return re.sub("[() -]", "", s)
 
+def clear(s):
+    return re.sub(" ", "", s)
+
 
 def merge_email_like_on_home_page(address):
     return "\n".join(filter(lambda x: x != "",
-                            filter(lambda x: x is not None, [address.email, address.email2, address.email3])))
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None, [address.email, address.email2,address.email3]))))
 
 
 def merge_phones_like_on_home_page(address):
