1dfa69f5ed2de523362186afe90710cbaee3ac1d
oleg <bubenshchikov@iskrauraltel.ru>
2017-07-02 08:36:36 +0500

    добавлены параметры командной строки

--- fixture/application.py
+++ fixture/application.py
@@ -1,24 +1,34 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
+
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.address import AddressHelper
 
 
 class Application(object):
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError(f"Unrecognized browser {browser}")
+
         # self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.address = AddressHelper(self)
+        self.base_url = base_url
         self.open_home_page()
 
     def open_home_page(self):
         # open homepage
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/") and wd.find_element_by_name("searchstring")):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
 
     def is_valid(self):
