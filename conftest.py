1fcb2539fb63ea725787f0c0314c41e794deffa7
oleg <bubenshchikov@iskrauraltel.ru>
2017-07-05 13:45:14 +0500

    чтение базовых значений из конфигурационного файла

--- conftest.py
+++ conftest.py
@@ -2,22 +2,27 @@
 from idlelib import browser
 
 import pytest
-
+import json
+import os.path
 from fixture.application import Application
 
 fixture = None
+target = None
+
 
 @pytest.fixture()
 def app(request):
     global fixture
-    browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    global target
+    brows = request.config.getoption("--browser")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=brows, base_url=target["baseUrl"])
+
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
@@ -32,4 +37,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
