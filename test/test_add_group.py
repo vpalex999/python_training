02440d03036ccc6a6d50167e212481b2959c2ae8
oleg <bubenshchikov@iskrauraltel.ru>
2017-07-31 14:46:33 +0500

    добавил allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from random import randrange
-
+import pytest
 from model.group import Group
 # from data.groups import testdata
 
@@ -10,12 +10,15 @@ from model.group import Group
 
 def test_add_group(app, db, json_groups, check_ui):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert len(old_groups) == len(new_groups)
-    if check_ui:
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with pytest.allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step(f'When I add a group {group} to the list'):
+        app.group.create(group)
+    with pytest.allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert len(old_groups) == len(new_groups)
+        if check_ui:
+            assert sorted(old_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
